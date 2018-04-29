da430d2850581e8ef1decfda7a775e939e0fd19b
github username <github email address>
2018-04-29 16:45:54 -0700

    generalized the array and elements to fit assignment

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,33 +3,33 @@ def create_an_empty_array
 end
 
 def create_an_array
-  instruments = ["violin", "viola", "cello", "bass"]
+  array = ["violin", "viola", "cello", "bass"]
 end
 
 def add_element_to_end_of_array(array, element)
-  instruments.push("bassoon")
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  instruments.unshift("marimba")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  instruments.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  instruments.shift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  instruments[1]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  instruments.first
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-  instruments.last
+  array.last
 end
