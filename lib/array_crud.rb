56539a0515e4c5354058c990cf22a30b2b532ad3
Laura Adam <laurawynosky@gmail.com>
2018-05-30 23:40:02 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,40 +1,38 @@
 def create_an_empty_array
-  create_an_empty_array = Array.new
-   puts create_an_empty_array.inspect
+  create_an_empty_array = []
 end
 
 def create_an_array
-  create_an_array= ["skis", "boots", "poles","helmet"]
+   create_an_array= ["skis", "boots", "poles","helmet"]
 end
 
 def add_element_to_end_of_array(array, element)
-  snow_array << "gloves"
-   puts snow_array.inspect
+  create_an_array << "gloves"
+   puts create_an_array.inspect
 end
 
 def add_element_to_start_of_array(array, element)
-  snow_array.unshift ("jacket")
-  puts snow_array.inspect
+  create_an_array.unshift ("jacket")
+  puts create_an_array.inspect
 end
 
 def remove_element_from_end_of_array(array)
-  snow_array.pop
-   puts snow_array.inspect
+  create_an_array.pop
+   puts create_an_array.inspect
 end
 
 def remove_element_from_start_of_array(array)
-  snow_array.shift ("jacket")
-   puts snow_array.inspect
+  create_an_array.shift ("jacket")
 end
 
 def retrieve_element_from_index(array, index_number)
-  snow_array[3]
+  create_an_array [3]
 end
 
 def retrieve_first_element_from_array(array)
-  snow_array.first
+  create_an_array.first
 end
 
 def retrieve_last_element_from_array(array)
-  snow_array.last
+  create_an_array.last
 end
