7cf3899902e7ed0d115ab037e543b5495659ecb2
Pete Hanner <petehanner@gmail.com>
2019-05-13 20:21:38 -0400

    Learned about arrays

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  meats = ["beef","chicken","pork","fish"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  capitals = ["DC", "London", "Paris"]
+  capitals << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  truth = ["arrays", "are", "incredible"]
+  truth.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  truth = ["I", "love", "arrays!"]
+  truth.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  truth = ["wow", "arrays", "are", "incredible"]
+  truth.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  me = ["I", "am", "learning", "about", "arrays"]
+  me[1]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  truth = ["wow", "arrays", "are", "incredible"]
+  truth[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  truth = ["I", "love", "arrays!"]
+  truth[-1]
 end
